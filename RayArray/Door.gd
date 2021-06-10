498b9b3ea3b93ec5079e45aac936df4ce034636b
Call-Me-Sir <82476534+Call-Me-Sir@users.noreply.github.com>
2021-06-11 10:57:56 +1200

    Some Doors stuff

--- RayArray/Door.gd
+++ RayArray/Door.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
-
-
+var open = false
+onready var line = $Line2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,13 +8,24 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	line.clear_points()
+	line.add_point(Vector2.ZERO)
+	
+	line.add_point($Polygon2D.global_position) # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
+	#if not _on_sensor_RayHit():
+		#open == false
+		#$AnimationPlayer.play_backwards("DoorOpen")
 
 
 func _on_Sensor_RayHit():
+	if open == false:
+		$AnimationPlayer.play("DoorOpen")
+		open = true
 	print("Hit!") # Replace with function body.
+	
+	
